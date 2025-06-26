7f869e40a0c63d45d1a2a32663c7688aa55acf58
Kayky Júnio <dev.kayky33@gmail.com>
2025-06-26 00:25:09 -0300

    feat: Delete method and filter implementation

--- produtos/urls.py
+++ produtos/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.listar_produtos, name='listar_produtos'),
     path('novo/', views.cadastrar_produto, name='cadastrar_produto'),
     path('editar/<int:id>/', views.editar_produto, name='editar_produto'),
+    path('excluir/<int:id>/', views.excluir_produto, name='excluir_produto'),
+    path('baixar/<int:id>/', views.baixar_estoque, name='baixar_estoque'),
 ]
